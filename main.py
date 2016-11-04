6f42efb0420f5302742c883968460798c234de94
Xiao Di Guan <xdg@puxlit.net>
2016-11-04 11:33:08 +1100

    Respect the game name length limit

--- main.py
+++ main.py
@@ -13,6 +13,110 @@ import scapy_http.http as scapy_http
 logger = logging.getLogger('integration')
 
 
+def truncate(string, max_bytes):
+    """
+    Truncates a string to no longer than the specified number of bytes.
+
+    >>> truncate('foobar', 8)
+    'foobar'
+
+    Lob off "partial" words, where practical:
+    >>> truncate('lorem ipsum dolor sit amet', 21)
+    'lorem ipsum […]'
+    >>> truncate('lorem ipsum dolor sit amet', 22)
+    'lorem ipsum […]'
+    >>> truncate('lorem ipsum dolor sit amet', 23)
+    'lorem ipsum dolor […]'
+
+    Otherwise, break apart the word:
+    >>> truncate('howdeedoodeethere', 11)
+    'howdee[…]'
+    """
+
+    # These should really be constants, but meh…
+    ellipsis = '[…]'
+    space = ' '
+    max_bytes_available_when_truncated = max_bytes - len(ellipsis.encode())
+    assert max_bytes_available_when_truncated >= 0
+
+    # If we're within budget, brill…
+    if len(string.encode()) <= max_bytes:
+        return string
+
+    # Roughly cut things down to size…
+    string = string[:max_bytes_available_when_truncated]
+    # Shave off additional characters, if necessary…
+    while len(string.encode()) > max_bytes_available_when_truncated:
+        string = string[:-1]
+    # If the string ends with a "partial" word, then lob that off…
+    if not string[-1].isspace():
+        split = string.rsplit(maxsplit=1)
+        if len(split) == 2:
+            string = split[0] + space
+    # Finally, tack on the ellipsis, and call it a day…
+    truncated_string = string + ellipsis
+    assert len(truncated_string.encode()) <= max_bytes
+    return truncated_string
+
+
+def format_status_message(artist, title):
+    r"""
+    Formats the supplied artist and title into an acceptable user presence game name.
+
+    >>> format_status_message('audiomachine', 'Beyond the Clouds')
+    '♪ audiomachine — Beyond the Clouds'
+
+    If the combo is too long, start by cutting the artist down to size:
+    >>> format_status_message(
+    ...     'Daniel Winiger, Daniel Perret & Praxedis Rutti',
+    ...     'Ellen\'s Gesang III (Ave Maria!), Op. 56, No. 6, D. 839, "Hymne an Die Jungfrau"'
+    ... )
+    '♪ Daniel Winiger, Daniel Perret & […] — Ellen\'s Gesang III (Ave Maria!), Op. 56, No. 6, D. 839, "Hymne an Die Jungfrau"'
+
+    If that's still not enough, then trim the title:
+    >>> format_status_message(
+    ...     'Gustav Kuhn, Haydn Orchestra of Bolzano & Trento & Soloists of Accademia di Montegral',
+    ...     'Symphonie No. 6 in A-Dur, Op. 68 - "Pastorale": Erwachen heiterer Gefühle bei der Ankunft auf dem Lande: Allegro ma non troppo'
+    ... )
+    '♪ Gustav Kuhn, Haydn […] — Symphonie No. 6 in A-Dur, Op. 68 - "Pastorale": Erwachen heiterer Gefühle bei der […]'
+
+    Placeholders are used for effectively blank artists and/or titles:
+    >>> format_status_message('', ' ')
+    '♪ [unknown] — [unknown]'
+    """
+
+    # These should really be constants, but meh…
+    prefix = '♪ '
+    separator = ' — '
+    unknown = '[unknown]'
+    min_artist_bytes_when_truncated = 30
+    max_bytes = 128
+    max_bytes_available = max_bytes - len(prefix.encode()) - len(separator.encode())
+    assert min_artist_bytes_when_truncated < max_bytes_available  # Remember, titles take up at least a byte.
+
+    artist = artist.strip() or unknown
+    title = title.strip() or unknown
+
+    artist_bytes = len(artist.encode())
+    title_bytes = len(title.encode())
+    bytes_left = max_bytes_available - artist_bytes - title_bytes
+
+    # If we're over budget…
+    if bytes_left < 0:
+        # … cut the artist down to size, if necessary…
+        if artist_bytes > min_artist_bytes_when_truncated:
+            artist = truncate(artist, max(min_artist_bytes_when_truncated, artist_bytes + bytes_left))
+            artist_bytes = len(artist.encode())
+            bytes_left = max_bytes_available - artist_bytes - title_bytes
+        # … then cut the title down to size, if necessary…
+        if bytes_left < 0:
+            title = truncate(title, title_bytes + bytes_left)
+
+    status_message = prefix + artist + separator + title
+    assert len(status_message.encode()) <= max_bytes
+    return status_message
+
+
 def intercept_lastfm_requests(queue):
     grace_period = 2
     timer = None
@@ -40,7 +144,7 @@ def intercept_lastfm_requests(queue):
                     payload = parse_qs(http_request.load.decode())
                     (artist, title, length) = (payload['a'][0], payload['t'][0], int(payload['l'][0]))
                     logger.info('Intercepted now playing POST for «{:s}» by «{:s}»; will dispatch status message, and clear in {:d} + {:d} seconds'.format(title, artist, length, grace_period))
-                    queue.put('♪ {:s} — {:s}'.format(artist, title))
+                    queue.put(format_status_message(artist, title))
                     clear_status_message_later(length + grace_period)
                 elif http_request.Path == b'/protocol_1.2':
                     logger.info('Intercepted scrobbling POST; will clear status message in {:d} seconds'.format(grace_period))
